28107785ee1d207e048172a0925d398ed7c85727
Marek Lukáš <tajnymag@gmail.com>
2018-11-20 15:02:26 +0100

    Fixed the case of answers which we don't have correct answers to

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,6 +1,4 @@
 <template>
-
-
     <div class="card text-center">
         <div class="card-header">
             Otázka
@@ -8,56 +6,65 @@
         <div class="card-body">
             <h5 class="card-title">{{currentQuestion.text}}</h5>
 
-            <button class="btn btn-primary" v-if="!correct && !mistake" @click="submitAnswer(true)">Ano</button>
-            <button class="btn btn-primary" v-if="!correct && !mistake" @click="submitAnswer(false)">Ne</button>
+            <button class="btn btn-primary" v-if="!correct && !mistake && !unknown" @click="submitAnswer(true)">Ano</button>
+            <button class="btn btn-primary" v-if="!correct && !mistake && !unknown" @click="submitAnswer(false)">Ne</button>
             <button class="btn btn-primary" @click="nextQuestion">Další</button>
         </div>
-        <div class="card-footer text-muted alert" v-bind:class="{ 'alert-success': correct, 'alert-danger': mistake}">
+        <div class="card-footer text-muted alert"
+             v-bind:class="{ 'alert-success': correct, 'alert-danger': mistake, 'alert-warning': unknown}">
             <span v-if="correct">Správně</span>
             <span v-else-if="mistake">Špatně</span>
-            <span v-else>Nezodpovězeno</span>
+            <span v-else-if="unknown">Spekulativní. Pokud jsi si jist(á) svou odpovědí, pošli mi mail na xlukm014@studenti.czu.cz</span>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {QuestionSet, Question} from '../question-set.ts';
+    import Vue from 'vue';
+    import {QuestionSet, Question} from '../question-set.ts';
 
-export default Vue.extend({
-    data() {
-        return {
-            currentQuestion: {
-                text: 'Načítám...',
-                answer: null
-            },
-            correct: false,
-            mistake: false,
-            questionSet: QuestionSet
-        }
-    },
-    mounted() {
-        this.questionSet = new QuestionSet();
-        this.currentQuestion = this.questionSet.getRandomUnanswered();
-    },
-    methods: {
-        submitAnswer(answer: boolean) {
-            const answeredCorrectly = (this.currentQuestion.answer === answer);
-
-            if (answeredCorrectly) {
-                this.correct = true;
-                this.mistake = false;
-            } else {
-                this.correct = false;
-                this.mistake = true;
+    export default Vue.extend({
+        data() {
+            return {
+                currentQuestion: {
+                    text: 'Načítám...',
+                    answer: null
+                },
+                correct: false,
+                mistake: false,
+                unknown: false,
+                questionSet: QuestionSet
             }
-            this.questionSet.setAnswered(this.currentQuestion, answeredCorrectly);
         },
-        nextQuestion() {
+        mounted() {
+            this.questionSet = new QuestionSet();
             this.currentQuestion = this.questionSet.getRandomUnanswered();
-            this.correct = false;
-            this.mistake = false;
+        },
+        methods: {
+            resetCurrentAnswer() {
+                this.correct = false;
+                this.mistake = false;
+                this.unknown = false;
+            },
+            submitAnswer(answer: boolean) {
+                this.resetCurrentAnswer();
+                const answeredCorrectly = (this.currentQuestion.answer === answer);
+
+                if ([null, undefined].includes(this.currentQuestion.answer)) {
+                    this.unknown = true;
+                    this.questionSet.setAnswered(this.currentQuestion, false);
+                    return;
+                } else if (answeredCorrectly) {
+                    this.correct = true;
+                } else {
+                    this.mistake = true;
+                }
+                this.questionSet.setAnswered(this.currentQuestion, answeredCorrectly);
+            },
+            nextQuestion() {
+                this.currentQuestion = this.questionSet.getRandomUnanswered();
+                this.resetCurrentAnswer();
+            }
         }
-    }
-})
+    })
 </script>
