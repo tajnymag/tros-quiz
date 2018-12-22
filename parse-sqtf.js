e5252400bcd84b8a9327c2987b620278e58fd976
Tajnymag <tajnymag@gmail.com>
2018-12-22 11:56:39 +0100

    Added parse-sqtf to be able to save directly parsed questions

--- parse-sqtf.js
+++ parse-sqtf.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const {execSync} = require('child_process');
 
 const filePath = path.resolve(__dirname, process.argv[2]);
 const questions = fs.readFileSync(filePath, { encoding: 'utf8' }).split('\n');
+const shouldWrite = ['-w', '--write'].includes(process.argv[3]);
 
 function toBoolean(word) {
 	return ['ANO', 'TRUE', '1', 'JOP', 'A', 'T'].includes(
@@ -33,5 +35,10 @@ questions
 	.forEach(line => {
 		const { message, answer } = parseAndFixLine(line);
 
-		console.log(JSON.stringify(message), answer);
+		if (shouldWrite) {
+			execSync(`node ${__dirname + "/dataset.js"} add ${JSON.stringify(message)} ${answer}`);
+			console.log(`node ${__dirname + "/dataset.js"} add ${JSON.stringify(message)} ${answer}`);
+		} else {
+			console.log(JSON.stringify(message), answer);
+		}
 	});
